c76b16fec1e53ac9f5d1cae084c73424fa6591b8
“Hatuongnguyen” <hatuongnguyen0107@gmail.com>
2024-02-18 19:36:14 +0700

    Formatting code, adding comment and docstring in nguyenpanda/__init__.py

--- nguyenpanda/nguyenpanda/__init__.py
+++ nguyenpanda/nguyenpanda/__init__.py
@@ -1,14 +1,21 @@
 """
-Welcome to the world of nguyenpanda, a Python library that brings you the essence of utility packages, each named after animals, perfectly representing its unique domain.
+Welcome to the world of nguyenpanda,
+    a Python library that brings you the essence of utility packages,
+    each named after animals, perfectly representing its unique domain.
 
 🦋 butterfly
-Explore the randomness and chaos with the mysterious butterfly package. Dive into the world of uncertainty, probability, and statistics. Embrace the unexpected with nguyenpanda's butterfly.
+    Explore the randomness and chaos with the mysterious butterfly package.
+    Dive into the world of uncertainty, probability, and statistics.
+    Embrace the unexpected with nguyenpanda's butterfly.
 
 🦉 owl
-Uncover the secrets of mathematics with the wise owl package. Master calculus, conquer linear algebra, and navigate through mathematical landscapes. Let nguyenpanda's owl guide you on your intellectual journey.
+    Uncover the secrets of mathematics with the wise owl package.
+    Master calculus, conquer linear algebra, and navigate through mathematical landscapes.
+    Let nguyenpanda's owl guide you on your intellectual journey.
 
 🦢 swan
-Indulge in the beauty of aesthetics and user interfaces with the elegant swan package. Dive into the world of colors, front-end development, GUI, and more. Transform your applications into visual masterpieces with swan.
+    Indulge in the beauty of aesthetics and user interfaces with the elegant swan package.
+    Dive into the world of colors, front-end development, GUI, and more. Transform your applications into visual masterpieces with swan.
 """
 
 __version__ = '0.1.6'
