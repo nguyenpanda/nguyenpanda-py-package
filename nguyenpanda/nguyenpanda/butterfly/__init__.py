3a2bfad83d2b7a0c34af576f4b933587ee65100d
“Hatuongnguyen” <hatuongnguyen0107@gmail.com>
2024-02-18 19:30:42 +0700

    Adding docstring in butterfly/__init__.py

--- nguyenpanda/nguyenpanda/butterfly/__init__.py
+++ nguyenpanda/nguyenpanda/butterfly/__init__.py
@@ -1,3 +1,11 @@
+"""
+butterfly module contains anything related to randomness, probability, and statistics.
+Small things can have non-linear impacts on a complex system, so don't let the butterfly fly away.
+
+Classes:
+    - RandomORG module for interacting with the Random.org API.
+"""
+
 from .RandomORG import RandomORG
 
 __all__ = [
