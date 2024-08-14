c32630921f17533f50682b2a9501c1a179356b88
Hatuongnguyen <hatuongnguyen0107@gmail.com>
2024-08-14 22:37:37 +0700

    Add `ant` package

--- nguyenpanda/nguyenpanda/ant/jupyter_nb.py
+++ nguyenpanda/nguyenpanda/ant/jupyter_nb.py
@@ -2,8 +2,16 @@ import os
 from pathlib import Path
 
 
-def create_alias(source_path, alias_name, alias_path: Path | str = Path.cwd()):
+def create_alias(source_path: str | Path, alias_name: str, alias_path: str | Path = Path.cwd()) -> Path:
+    """
+
+    :param source_path: Where the true directory located
+    :param alias_name: The alias name
+    :param alias_path: Specify the path of the alias
+    :return: The path to alias
+    """
     alias_path = (Path(alias_path) / alias_name).absolute()
+    source_path = Path(source_path).absolute()
 
     if alias_path.is_dir():
         print('\033[1;93mDirectory \"\033[1;92m{}\033[1;93m\" already exist!\033[0m'.format(alias_path))
